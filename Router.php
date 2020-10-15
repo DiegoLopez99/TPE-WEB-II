e5d8ea313fe5824c8ea1dacfbbdb6f6d36378229
DiegoLopez99 <42392803+DiegoLopez99@users.noreply.github.com>
2020-10-15 18:45:24 -0300

    Add files via upload

---
+++ Router.php
@@ -0,0 +1,33 @@
+<?php
+    require_once "controller/Controller.php";
+    require_once "RouterClass.php";
+
+    define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+    define('LOGIN', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/login');
+    define('ADMINISTRAR', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/administar');
+
+    $r = new Router();
+
+    //Rutas
+    $r->addRoute("home", "GET", "Controller", "Home");
+    $r->addRoute("tienda", "GET", "Controller", "Tienda");
+    $r->addRoute("generos", "GET", "Controller", "Generos");
+    $r->addRoute("login", "GET", "Controller", "Login");
+    $r->addRoute("verifyUser", "POST", "Controller", "VerifyUser");
+    $r->addRoute("administrar", "GET", "Controller", "Administrar");
+    $r->addRoute("eliminar/:ID", "GET", "Controller", "DeleteGame");
+    $r->addRoute("enStock/:ID", "GET", "Controller", "EnStock");
+    $r->addRoute("sinStock/:ID", "GET", "Controller", "SinStock");
+    $r->addRoute("logout", "GET", "Controller", "Logout");
+    $r->addRoute("tabla/:ID", "GET", "Controller", "MostrarTabla");
+    $r->addRoute("addGame", "POST", "Controller", "AddGame");
+    $r->addRoute("eliminarGenero/:ID", "GET", "Controller", "EliminarGenero");
+    $r->addRoute("addGenero", "POST", "Controller", "AddGenero");
+    $r->addRoute("juego/:ID", "GET", "Controller", "MostrarJuego");
+
+    //Ruta por defecto.
+    $r->setDefaultRoute("Controller", "Home");
+
+    //run
+    $r->route($_GET['action'], $_SERVER['REQUEST_METHOD']);
+?>
---
+++ dl_gameshop.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-10-2020 a las 23:34:02
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dl_gameshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `genero`
+--
+
+CREATE TABLE `genero` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`id`, `nombre`) VALUES
+(1, 'Deportes'),
+(2, 'Accion'),
+(3, 'Conduccion'),
+(4, 'Aventura'),
+(5, 'Estrategia');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `juego`
+--
+
+CREATE TABLE `juego` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `precio` double NOT NULL,
+  `formato` varchar(255) NOT NULL,
+  `id_genero` int(11) NOT NULL,
+  `stock` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `juego`
+--
+
+INSERT INTO `juego` (`id`, `nombre`, `precio`, `formato`, `id_genero`, `stock`) VALUES
+(2, 'FIFA 21', 4500, 'Digital', 1, 1),
+(3, 'Mafia Trilogy', 6120, 'Digital', 2, 1),
+(10, 'Sims 4', 4500, 'Fisico', 5, 0),
+(15, 'Call Of Duty Black Ops Cold War', 3800, 'Digital', 2, 1),
+(16, 'Dirt 5', 6100, 'Fisico', 3, 1),
+(17, 'F1 2020 Seventy Ediion', 4440, 'Digital', 3, 1),
+(22, 'Need for Speed Heat', 5420, 'Fisico', 3, 1),
+(23, 'ARK Survival Evolved', 1560, 'Digital', 4, 1),
+(24, 'Crash Bandicoot 4 Its About', 6340, 'Fisico', 4, 1),
+(25, 'Fall Guys Ultimate Knockout', 1320, 'Digital', 4, 1),
+(26, 'Minecraft', 1536, 'Digital', 4, 1),
+(27, 'Jurassic World Evolution', 2300, 'Fisico', 4, 0),
+(28, 'NBA 2K21', 4300, 'Digital', 1, 1),
+(29, 'UFC 4', 6350, 'Fisico', 1, 1),
+(30, 'eFootball PES 2021', 2560, 'Digital', 1, 1),
+(31, 'GTA V', 1130, 'Digital', 2, 1),
+(32, 'Mortal Kombat 11', 4400, 'Fisico', 2, 1),
+(33, 'Red Dead Redemption 2', 3100, 'Fisico', 2, 1),
+(34, 'The Last of Us Part II ', 6400, 'Fisico', 2, 0),
+(35, 'HITMAN The Complete First Season', 1320, 'Digital', 5, 1),
+(37, 'FEAR', 600, 'Fisico', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `admin`) VALUES
+(1, 'diego@gmail.com', '$2y$10$BcX4HeOuhiUTwPHpNlP6A.GycT/N/HkbO7Rd.AVZKSXxzmNZBjGiW', 1),
+(2, 'javi@gmail.com', '$2y$10$IKzG6zJ0CiXLcHlAnSgcD.4Wkwv6zd9MACRzWXjZjn9cJgpXPVMIu', 1),
+(3, 'roberto@gmail.com', '$2y$10$GCfjYhBDb0qh/.55LGw7we42HhhZBBdpTb05g3PWzXKlLA/FAqEHa', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `juego`
+--
+ALTER TABLE `juego`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_id_genero` (`id_genero`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `genero`
+--
+ALTER TABLE `genero`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `juego`
+--
+ALTER TABLE `juego`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `juego`
+--
+ALTER TABLE `juego`
+  ADD CONSTRAINT `juego_ibfk_1` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
